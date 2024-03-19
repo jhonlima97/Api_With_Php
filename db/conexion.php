4ec9a2b133780af91201bf73e55eaefb0fad05ee
Jhon Wayler Camizan <jhonw.lima08@gmail.com>
2024-03-19 01:22:00 -0500

    remove credentials

--- db/conexion.php
+++ db/conexion.php
@@ -1,13 +1,18 @@
 <?php
 
-$hostname   ="localhost";
-$username   ="root";
-$passname   ="ADMINPHP";
-$dbname     ="api_php";
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$conexion = new mysqli($hostname,$username,$passname,$dbname);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 
-if($conexion->connect_error){
+$hostname = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$passname = $_ENV['DB_PASS'];
+$dbname   = $_ENV['DB_NAME'];
+
+$conexion = new mysqli($hostname, $username, $passname, $dbname);
+
+if ($conexion->connect_error) {
     die("Error en la conexion". $conexion->connect_error);
 } else {
     //echo "Conexion establecida";
